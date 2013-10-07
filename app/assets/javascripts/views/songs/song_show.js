89808bc5b96767d32823e102e8c629039daba039
Jackson Popkin <jdpopkin@gmail.com>
2013-10-07 18:01:04 -0400

    Can now insert a span dynamically at end of highlighted text

--- app/assets/javascripts/views/songs/song_show.js
+++ app/assets/javascripts/views/songs/song_show.js
@@ -7,7 +7,8 @@ Notes.Views.SongShow = Backbone.View.extend({
 
   events: {
     // TODO: determine if it is safe to use JQuery's .on("select") and not this
-    "mouseup .lyrics": "handleSelect"
+    "mouseup .lyrics": "handleSelect",
+
   },
 
   render: function() {
@@ -21,7 +22,19 @@ Notes.Views.SongShow = Backbone.View.extend({
   },
 
   handleSelect: function(event) {
+    var that = this;
+    if ($(".add-note").length > 0) {
+      console.log($(".add-note"));
+      $(".add-note").remove();
+      var htmlCopy = $(".lyrics").html();
+      console.log(htmlCopy);
+      $(".lyrics").html(htmlCopy);
+      return;
+    }
+
     var sel = window.getSelection();
+
+    // handle case of highlighting outside bounds of lyrics
     if (sel.anchorNode !== sel.focusNode) {
       return;
     }
@@ -31,9 +44,27 @@ Notes.Views.SongShow = Backbone.View.extend({
     var range = sel.getRangeAt(0);
     var contents = range.cloneContents();
 
-    // console.log(sel);
-    // console.log(range);
-    // console.log(contents);
-    // console.log(sel.toString());
+    console.log(sel);
+    console.log(range);
+    console.log(contents);
+    console.log(sel.toString());
+
+    var lowEnd = sel.extentOffset < sel.baseOffset ? sel.extentOffset : sel.baseOffset;
+    var highEnd = sel.extentOffset < sel.baseOffset ? sel.baseOffset : sel.extentOffset;
+    console.log(lowEnd);
+    console.log(highEnd);
+
+    var htmlCopy = $(".lyrics").html();
+    htmlCopy = that.addButton(htmlCopy, highEnd);
+    $(".lyrics").html(htmlCopy);
+  },
+
+  addButton: function(htmlCopy, highEnd) {
+    var stringStart = htmlCopy.slice(0, highEnd);
+    var stringEnd = htmlCopy.slice(highEnd);
+
+    var buttonHtml = "<span class='add-note'>Add note!</span>";
+
+    return stringStart.concat(buttonHtml).concat(stringEnd);
   }
 })
