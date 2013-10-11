d87e0a03b7a8bce08cb0f27d06e46d7f955a1149
Jackson Popkin <jdpopkin@gmail.com>
2013-10-11 15:03:26 -0400

    Recent content now includes albums.

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,11 +1,13 @@
 module RootHelper
 
   def recent_content(n)
+    # TODO: this does not currently include albums
     # Artists Albums Songs Notes Comments. No to Votes, Users.
     big_array = Artist.includes(:user).last(n).to_a +
                 Song.includes(:artist, :user).last(n).to_a +
                 Note.includes(:song, :user).last(n).to_a +
-                Comment.includes(:commentable, :user).last(n).to_a
+                Comment.includes(:commentable, :user).last(n).to_a +
+                Album.includes(:artist, :user).last(n).to_a
 
     big_array.sort_by! { |obj| obj.created_at }
     big_array.last(n).reverse
