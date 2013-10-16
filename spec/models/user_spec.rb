4de42db2903348efdf1a1d86070729a4546388a0
Jackson Popkin <jdpopkin@gmail.com>
2013-10-16 17:36:15 -0400

    Adds another two tests to user spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,24 @@ describe User do
     expect(user.is_password?("password")).to be_true
   end
 
+  it "will return false when the password does not match" do
+    user = User.create(email: "asdf@asdf.com", password: "password",
+                      username: "Test User")
+
+    expect(user.is_password?("wrong_password")).to be_false
+  end
+
   it "can find a user given their username and password" do
     user = User.create(email: "asdf@asdf.com", password: "password",
                       username: "Test User")
 
-    expect(User.find_by_credentials("Test User", "password")).to be_true
+    expect(User.find_by_credentials("Test User", "password")).to be
+  end
+
+  it "will not return a user if password is incorrect" do
+    user = User.create(email: "asdf@asdf.com", password: "password",
+                      username: "Test User")
+
+    expect(User.find_by_credentials("Test User", "wrong_password")).to be_nil
   end
 end
