d54a5b5cc65d3d86332116bad4a48efa98ceab7e
Jackson Popkin <jdpopkin@gmail.com>
2013-10-16 11:48:32 -0400

    Adds a validation preventing users from voting on their own content.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,6 +6,21 @@ class Vote < ActiveRecord::Base
   validates :votable_id, presence: true
   validates :votable_type, presence: true
   validates :value, inclusion: [-1, 1]
+  validate :cannot_vote_on_own_votable
+
+  def cannot_vote_on_own_votable
+    klass = self.votable_type.constantize
+    # user_id = nil
+    if klass == Note
+      target_user = Note.find(self.votable_id).author_id
+    else
+      target_user = klass.find(self.votable_id).user_id
+    end
+
+    if target_user == self.user_id
+      errors[:base] << "Cannot vote on your own item"
+    end
+  end
 
   belongs_to :user
   belongs_to :votable, polymorphic: true
