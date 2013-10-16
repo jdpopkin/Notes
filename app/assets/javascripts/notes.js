f94eb3650eb6d7bd846d109626a9a71d5a81f6c9
Jackson Popkin <jdpopkin@gmail.com>
2013-10-16 16:58:54 -0400

    All scores are now signed.

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -5,6 +5,14 @@ window.Notes = {
   Routers: {},
   Artists: [],
   Albums: [],
+  toSigned: function(num) {
+    if (num < 0) {
+      return num.toString();
+    } else {
+      return "+" + num.toString();
+    }
+  },
+
   initialize: function(rootUrl) {
     Notes.rootUrl = rootUrl;
   }
