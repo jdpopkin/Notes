3b144d2a3c012b7bbd4b302eedcdb8db20f29c39
Jackson Popkin <jdpopkin@gmail.com>
2013-10-16 14:22:37 -0400

    Corrects vote tests.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -34,7 +34,6 @@ describe Album do
     expect(album.score).to eq(-1)
   end
 
-
   # integration tests
   it "should return the recent score of an album" do
     album = Album.create(user_id: 1, artist_id: 1, title: "Test Album")
@@ -48,12 +47,36 @@ describe Album do
     vote2 = Vote.create(user_id: 2, votable_id: song2.id, votable_type: "Song", value: 1)
     vote3 = Vote.create(user_id: 3, votable_id: song2.id, votable_type: "Song", value: 1)
 
-    p album.songs
-    p album.songs.last.votes
-    p vote2
-    p album.votes
-
     expect(album.recent_score).to eq(3)
+  end
+
+  it "should only count recent votes toward recent_score" do
+    album = Album.create(user_id: 1, artist_id: 1, title: "Test Album")
+    song1 = Song.create(user_id: 1, artist_id: 1, title: "Test Song",
+            lyrics: "Words", album_id: album.id)
+
+    song2 = Song.create(user_id: 1, artist_id: 1, title: "Test Song",
+            lyrics: "Words", album_id: album.id)
+
+    vote1 = Vote.create(user_id: 2, votable_id: song1.id, votable_type: "Song", value: 1)
+    vote2 = Vote.create(user_id: 2, votable_id: song2.id, votable_type: "Song", value: 1)
+    vote3 = Vote.create(user_id: 3, votable_id: song2.id, votable_type: "Song", value: 1)
+
+    vote1.created_at = 1.year.ago
+    vote2.created_at = 1.year.ago
+
+    vote1.save
+    vote2.save
+
+    expect(album.recent_score).to eq(1)
+  end
+
+  it "should return #score when #cached_score is called" do
+    album = Album.new(user_id: 1, artist_id: 1, title: "Test Album")
+
+    album.stub(:recent_score).and_return(3)
+
+    expect(album.cached_score).to eq(3)
   end
 
 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Vote do
 
   it "should not allow a user to vote on their own votable" do
-    vote = Vote.new(votable_id: 1, votable_type: "Comment", user_id: 1)
+    vote = Vote.new(votable_id: 1, votable_type: "Comment", user_id: 1, value: 1)
 
     comment1 = double("Comment")
     comment1.stub(:user_id).and_return(1)
@@ -14,4 +14,14 @@ describe Vote do
   end
 
   # should allow on others' votables
+  it "should allow a user to vote on others' votables" do
+    vote = Vote.new(votable_id: 1, votable_type: "Comment", user_id: 1, value: 1)
+
+    comment1 = double("Comment")
+    comment1.stub(:user_id).and_return(2)
+
+    vote.stub(:votable).and_return(comment1)
+
+    expect(vote).to be_valid
+  end
 end
